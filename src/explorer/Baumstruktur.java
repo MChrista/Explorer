3ff64a233cc2b3f24e10835a851e2f5efa4b932f
andi-93 <a-pregler@gmx.de>
2014-01-21 13:30:00 +0100

    Eclipse Projekt eingefuegt

---
+++ src/explorer/Baumstruktur.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package explorer;
+
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author A001278
+ */
+public class Baumstruktur extends JPanel{
+    
+}
---
+++ src/explorer/Vorschau.java
@@ -0,0 +1,17 @@
+package explorer;
+
+import java.io.File;
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author A001278
+ */
+public class Vorschau extends JPanel{
+    public Vorschau() {
+    	
+    }
+    public void reload(File file) {
+    	
+    }
+}
